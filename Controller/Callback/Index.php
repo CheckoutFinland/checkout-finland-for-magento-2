ddec918fb02be860140f7cd71331189f7763cdbb
Dominik Rohal <dominik.rohal@solteq.com>
2020-03-12 11:05:05 +0200

    SQMAGOPC-141 removed unnecessary dependencies

--- Controller/Callback/Index.php
+++ Controller/Callback/Index.php
@@ -4,12 +4,7 @@ namespace Op\Checkout\Controller\Callback;
 
 use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Action\Context;
-use Magento\Quote\Model\QuoteRepository;
-use Op\Checkout\Gateway\Validator\ResponseValidator;
 use Op\Checkout\Helper\ProcessPayment;
-use Op\Checkout\Model\CheckoutException;
-use Op\Checkout\Model\TransactionSuccessException;
-use Op\Checkout\Model\ReceiptDataProvider;
 
 /**
  * Class Index
@@ -22,21 +17,6 @@ class Index extends \Magento\Framework\App\Action\Action
     protected $session;
 
     /**
-     * @var ResponseValidator
-     */
-    protected $responseValidator;
-
-    /**
-     * @var ReceiptDataProvider
-     */
-    protected $receiptDataProvider;
-
-    /**
-     * @var QuoteRepository
-     */
-    protected $quoteRepository;
-
-    /**
      * @var ProcessPayment
      */
     private $processPayment;
@@ -45,24 +25,15 @@ class Index extends \Magento\Framework\App\Action\Action
      * Index constructor.
      * @param Context $context
      * @param Session $session
-     * @param ResponseValidator $responseValidator
-     * @param QuoteRepository $quoteRepository
-     * @param ReceiptDataProvider $receiptDataProvider
      * @param ProcessPayment $processPayment
      */
     public function __construct(
         Context $context,
         Session $session,
-        ResponseValidator $responseValidator,
-        QuoteRepository $quoteRepository,
-        ReceiptDataProvider $receiptDataProvider,
         ProcessPayment $processPayment
     ) {
         parent::__construct($context);
         $this->session = $session;
-        $this->responseValidator = $responseValidator;
-        $this->receiptDataProvider = $receiptDataProvider;
-        $this->quoteRepository = $quoteRepository;
         $this->processPayment = $processPayment;
     }
 
