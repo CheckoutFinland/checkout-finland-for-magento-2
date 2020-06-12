a7970ea7a787de3d7a3d97531281f6ddc088d7d8
Roope Salminen <roope.salminen@solteq.com>
2020-06-12 09:38:39 +0300

    SQMAGOPC-197 - Added exception classes

---
+++ Exceptions/CheckoutException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Op\Checkout\Exceptions;
+
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * Class CheckoutException
+ */
+class CheckoutException extends LocalizedException
+{
+
+}
---
+++ Exceptions/TransactionSuccessException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Op\Checkout\Exceptions;
+
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * Class TransactionSuccessException
+ */
+class TransactionSuccessException extends LocalizedException
+{
+
+}
